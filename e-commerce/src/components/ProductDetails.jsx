e0fdb3bb5123fc9bc2ac7ba099b5891175e9db9d
kadr56 <sh.munkhbaatar@gmail.com>
2023-01-23 13:33:17 +0800

    minor change

--- e-commerce/src/components/ProductDetails.jsx
+++ e-commerce/src/components/ProductDetails.jsx
@@ -1,40 +1,42 @@
-import ImageGallery from 'react-image-gallery'
+import ImageGallery from "react-image-gallery";
 import { useLocation } from "react-router-dom";
 import { Rating } from "react-simple-star-rating";
 
 function ProductDetails() {
   const location = useLocation();
   const product = location.state;
-  console.log(product);
+  const [orderQty, setOrderQty] = console.log(product);
   console.log(product.imgUrl);
   console.log("length review = ", product.length);
 
-  console.log(product.imgUrl)
+  console.log(product.imgUrl);
 
   const colors = product.color.map((col, index) => {
     let colorStyle = {
-      backgroundColor: `${col}`
-    }
-    console.log(colorStyle)
-    return <div className='color' key={index} style={colorStyle} >
-    </div >
+      backgroundColor: `${col}`,
+    };
+    console.log(colorStyle);
+    return <div className="color" key={index} style={colorStyle}></div>;
   });
 
   const sizes = product.size.map((s, index) => {
-    return <div className='size' key={index}> {s} </div>
-  })
+    return (
+      <div className="size" key={index}>
+        {" "}
+        {s}{" "}
+      </div>
+    );
+  });
 
   return (
     <div className="product-details container">
-
       <div className="img-container">
         <ImageGallery items={product.imgUrl} />
         {/* <img src={`../image/play.png`} alt="" /> */}
-
       </div>
       <div className="details-container">
-        <div className='title'>{product.title} </div>
-        <div className='price'>${product.price} </div>
+        <div className="title">{product.title} </div>
+        <div className="price">${product.price} </div>
         <div className="product-ratings">
           <Rating
             // onClick={handleRating}
@@ -45,31 +47,42 @@ function ProductDetails() {
             allowFraction={true}
             size={35}
           />
-          {product.reviews.length > 0 ? (< span className='space'>  {product.reviews.length} reviews</span>) : (<span className='space'>No reviews</span>)}
+          {product.reviews.length > 0 ? (
+            <span className="space"> {product.reviews.length} reviews</span>
+          ) : (
+            <span className="space">No reviews</span>
+          )}
         </div>
-        <div className='availability'>Availability: {product.quantity > 0 ? (< span className='space text-success'> In Stock </span>) : (<span className='space text-danger'>Out of stock</span>)}
+        <div className="availability">
+          Availability:{" "}
+          {product.quantity > 0 ? (
+            <span className="space text-success"> In Stock </span>
+          ) : (
+            <span className="space text-danger">Out of stock</span>
+          )}
         </div>
-        <div className='qty'>
-          {product.quantity > 0 ? (< span>  Hurry up! only {product.quantity} product left in stock!</span>) : (<span> {product.quantity} product left in stock!</span>)}
+        <div className="qty">
+          {product.quantity > 0 ? (
+            <span>
+              {" "}
+              Hurry up! only {product.quantity} product left in stock!
+            </span>
+          ) : (
+            <span> {product.quantity} product left in stock!</span>
+          )}
         </div>
-        <div className='color-container'>
-          Color: {colors}
-        </div>
+        <div className="color-container">Color: {colors}</div>
         <div>
-          {product.size.length > 0 && <div className='size-container'>Size:  {sizes} </div>}
+          {product.size.length > 0 && (
+            <div className="size-container">Size: {sizes} </div>
+          )}
         </div>
-        <div className='order-qty'>
+        <div className="order-qty">
           Quantity: <button>-</button> 1 <button>+</button>
         </div>
-
         {/* <img src={`../${product.imgUrl}`} alt="" /> */}
       </div>
-
-
-
-
-
-    </div >
+    </div>
   );
 }
 
